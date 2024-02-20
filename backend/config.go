61222e584b92320625cd8d4631057078850d635e
Lou Foster <louffoster@gmail.com>
2024-02-20 10:08:09 -0500

    added user ws cfg and include it in healthcheck

--- backend/config.go
+++ backend/config.go
@@ -5,8 +5,14 @@ import (
 	"log"
 )
 
+type userServiceCfg struct {
+	URL string
+	JWT string
+}
+
 type configData struct {
 	port        int
+	userService userServiceCfg
 	devAuthUser string
 	jwtKey      string
 }
@@ -16,6 +22,10 @@ func getConfiguration() *configData {
 	flag.IntVar(&config.port, "port", 8080, "Port to offer service on")
 	flag.StringVar(&config.jwtKey, "jwtkey", "", "JWT signature key")
 
+	// user service
+	flag.StringVar(&config.userService.URL, "userws", "", "URL for the user service")
+	flag.StringVar(&config.userService.JWT, "userjwt", "", "JWT for the user service")
+
 	// dev user
 	flag.StringVar(&config.devAuthUser, "devuser", "", "Authorized computing id for dev")
 
@@ -24,8 +34,15 @@ func getConfiguration() *configData {
 	if config.jwtKey == "" {
 		log.Fatal("Parameter jwtkey is required")
 	}
+	if config.userService.URL == "" {
+		log.Fatal("Parameter userws is required")
+	}
+	if config.userService.JWT == "" {
+		log.Fatal("Parameter userjwt is required")
+	}
 
 	log.Printf("[CONFIG] port          = [%d]", config.port)
+	log.Printf("[CONFIG] userws        = [%s]", config.userService.URL)
 	if config.devAuthUser != "" {
 		log.Printf("[CONFIG] devuser       = [%s]", config.devAuthUser)
 	}
--- backend/service.go
+++ backend/service.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
+	"net/url"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,15 +18,44 @@ import (
 // serviceContext contains common data used by all handlers
 type serviceContext struct {
 	Version     string
+	HTTPClient  *http.Client
+	UserService userServiceCfg
 	JWTKey      string
 	DevAuthUser string
+}
+
+// RequestError contains http status code and message for a failed HTTP request
+type RequestError struct {
+	StatusCode int
+	Message    string
 }
 
 // InitializeService sets up the service context for all API handlers
 func initializeService(version string, cfg *configData) *serviceContext {
 	ctx := serviceContext{Version: version,
 		JWTKey:      cfg.jwtKey,
+		UserService: cfg.userService,
 		DevAuthUser: cfg.devAuthUser}
+
+	log.Printf("INFO: create HTTP client...")
+	defaultTransport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 600 * time.Second,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	ctx.HTTPClient = &http.Client{
+		Transport: defaultTransport,
+		Timeout:   5 * time.Second,
+	}
+	log.Printf("INFO: HTTP Client created")
 
 	return &ctx
 }
@@ -44,7 +80,6 @@ func (svc *serviceContext) lookupVersion() map[string]string {
 	return vMap
 }
 
-// HealthCheck reports the health of the serivce
 func (svc *serviceContext) healthCheck(c *gin.Context) {
 	type hcResp struct {
 		Healthy bool   `json:"healthy"`
@@ -53,5 +88,68 @@ func (svc *serviceContext) healthCheck(c *gin.Context) {
 	hcMap := make(map[string]hcResp)
 	hcMap["libra3"] = hcResp{Healthy: true}
 
+	_, err := svc.sendGetRequest(fmt.Sprintf("%s/version", svc.UserService.URL))
+	if err != nil {
+		hcMap["user-ws"] = hcResp{Healthy: false, Message: err.Message}
+	} else {
+		hcMap["user-ws"] = hcResp{Healthy: true}
+	}
+
 	c.JSON(http.StatusOK, hcMap)
+}
+
+func (svc *serviceContext) sendGetRequest(url string) ([]byte, *RequestError) {
+	return svc.sendRequest("GET", url, nil)
+}
+
+func (svc *serviceContext) sendRequest(verb string, url string, payload *url.Values) ([]byte, *RequestError) {
+	log.Printf("INFO: %s request: %s", verb, url)
+	startTime := time.Now()
+
+	var req *http.Request
+	if verb == "POST" && payload != nil {
+		req, _ = http.NewRequest("POST", url, strings.NewReader(payload.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	} else {
+		req, _ = http.NewRequest(verb, url, nil)
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36")
+	}
+
+	rawResp, rawErr := svc.HTTPClient.Do(req)
+	resp, err := handleAPIResponse(url, rawResp, rawErr)
+	elapsedNanoSec := time.Since(startTime)
+	elapsedMS := int64(elapsedNanoSec / time.Millisecond)
+
+	if err != nil {
+		log.Printf("ERROR: Failed response from %s %s - %d:%s. Elapsed Time: %d (ms)",
+			verb, url, err.StatusCode, err.Message, elapsedMS)
+	} else {
+		log.Printf("INFO: Successful response from %s %s. Elapsed Time: %d (ms)", verb, url, elapsedMS)
+	}
+	return resp, err
+}
+
+func handleAPIResponse(logURL string, resp *http.Response, err error) ([]byte, *RequestError) {
+	if err != nil {
+		status := http.StatusBadRequest
+		errMsg := err.Error()
+		if strings.Contains(err.Error(), "Timeout") {
+			status = http.StatusRequestTimeout
+			errMsg = fmt.Sprintf("%s timed out", logURL)
+		} else if strings.Contains(err.Error(), "connection refused") {
+			status = http.StatusServiceUnavailable
+			errMsg = fmt.Sprintf("%s refused connection", logURL)
+		}
+		return nil, &RequestError{StatusCode: status, Message: errMsg}
+	} else if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		defer resp.Body.Close()
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		status := resp.StatusCode
+		errMsg := string(bodyBytes)
+		return nil, &RequestError{StatusCode: status, Message: errMsg}
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	return bodyBytes, nil
 }
