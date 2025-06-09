7cec7c064eaff1b03221e01d345b7448657cb814
Lou Foster <louffoster@gmail.com>
2025-06-09 15:25:37 -0400

    admins see all licenes in the rights dropdown

--- frontend/src/views/EditWorkForm.vue
+++ frontend/src/views/EditWorkForm.vue
@@ -80,7 +80,8 @@
                <LabeledInput label="Notes" name="work.notes" v-model="etdRepo.work.notes" type="textarea" />
                <LabeledInput v-if="user.isAdmin" label="Admin Notes" name="work.adminNotes" v-model="etdRepo.work.adminNotes" type="textarea" />
                <div class="field" >
-                  <LabeledInput label="Rights" name="licenseID" :required="true" v-model="etdRepo.licenseID" type="select" :options="system.userLicenses" />
+                  <LabeledInput v-if="user.isAdmin" label="Rights" name="licenseID" :required="true" v-model="etdRepo.licenseID" type="select" :options="system.licenses" />
+                  <LabeledInput v-else label="Rights" name="licenseID" :required="true" v-model="etdRepo.licenseID" type="select" :options="system.userLicenses" />
                   <Message v-if="$form.licenseID?.invalid" severity="error" size="small" variant="simple">{{ $form.licenseID.error.message }}</Message>
                   <div class="note">
                      Libra lets you choose an open license when you post your work, and will prominently display the
