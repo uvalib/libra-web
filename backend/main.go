fecca83367b07f7fa92b235d2cef81bfee8eb093
Lou Foster <louffoster@gmail.com>
2025-07-09 10:30:03 -0400

    undo error handling change

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
@@ -74,10 +73,9 @@ func main() {
 	// by node/vite and it proxies all requests to the API to the routes above
 	router.Use(static.Serve("/", static.LocalFile("./public", true)))
 
-	// add a catchall route that returns 404
+	// add a catchall route that renders the index page.
 	router.NoRoute(func(c *gin.Context) {
-		log.Printf("ERROR: invalid request: %s", c.Request.URL.String())
-		c.String(http.StatusNotFound, "The resource you requested cannot be found")
+		c.File("./public/index.html")
 	})
 
 	portStr := fmt.Sprintf(":%d", cfg.port)
